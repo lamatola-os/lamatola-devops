26231e5f9e5835895dccde39afdec3a3a1a89841
Upadhyay <prayag.upd@gmail.com>
2020-02-25 20:38:13 -0800

    execute ssh command

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 
 ```
 pip3 install boto3
+pip3 install pick
 ```
--- list.py
+++ list.py
@@ -1,6 +1,10 @@
 import boto3
 ec2 = boto3.resource('ec2')
 
+from pick import pick
+import base64
+import os
+
 def get_name(instance):
     for tag in instance.tags:
         if tag['Key'] == 'Name':
@@ -9,6 +13,17 @@ def get_name(instance):
 
 instances = ec2.instances.filter(
     Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
-for instance in instances:
-    print(get_name(instance).ljust(50), instance.private_ip_address.ljust(10))
 
+ssh_vms = []
+vms = []
+
+for instance in instances:
+    # print(get_name(instance).ljust(50), instance.private_ip_address.ljust(10))
+    ssh_vm = 'ssh ' + instance.private_ip_address
+    ssh_vms.append(get_name(instance) + ' > ' + ssh_vm)
+    vms.append(ssh_vm)
+
+ssh_, index = pick(ssh_vms, "Select VM you want to ssh")
+
+print(vms[index])
+os.system(vms[index])
